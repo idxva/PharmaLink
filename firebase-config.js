fe623c19bb76afa3126234115d1ebcf8934e8766
Deva <devarajandeva57@gmail.com>
2026-09-05 20:35:45 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+// This file is loaded by index.html before script.js
+// Note: These are example credentials. Replace with your actual Firebase project credentials.
+
+window.firebaseConfig = {
+    apiKey: "AIzaSyBRJ9kWA5l2UQaq9iwPtJBnZmz434bTbbA",
+    authDomain: "pharmacylogs.firebaseapp.com",
+    projectId: "pharmacylogs",
+    storageBucket: "pharmacylogs.firebasestorage.app",
+    messagingSenderId: "1056107082724",
+    appId: "1:1056107082724:web:cd5547d7e6773719e38df1"
+};
